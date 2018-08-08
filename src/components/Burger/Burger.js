8604cbb8241a9253b10b704f1c6ed0e70e815e2c
Łukasz Ustach <lukasz.ustach@gmail.com>
2018-08-08 19:16:19 +0200

    add control component,and view elements

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,16 +3,21 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger=(props)=>{
-const transformedIngredient=Object.keys(props.ingredients).map(idKey=>{
-  return [...Array(props.ingredients[idKey])].map((_,i)=>{
-    <BurgerIngredient key={idKey+i} type={idKey}/>
-  })
-});
+let transformedIngredient=Object.keys(props.ingredients).map(igKey=>{
+  return [...Array(props.ingredients[igKey])].map((_,i)=>{
+    return <BurgerIngredient key={igKey+i} type={igKey}/>
+  });
+})
+  .reduce((arr,el)=>{
+    return arr.concat(el);
+  },[]);
+  if(transformedIngredient.length===0){
+    transformedIngredient=<p>Please start adding ingredients</p>
+  }
   return(
     <div className={classes.Burger}>
         <BurgerIngredient type='bread-top'/>
-        <BurgerIngredient type='cheese'/>
-        <BurgerIngredient type='meat'/>
+          {transformedIngredient}
         <BurgerIngredient type='bread-bottom'/>
      </div>
   );
