f2155b8975f479cb43c652835fc562c32915286c
Łukasz Ustach <lukasz.ustach@gmail.com>
2018-08-09 10:42:51 +0200

    add and delete function,disabled click

--- src/containers/BurgerBuilder.js
+++ src/containers/BurgerBuilder.js
@@ -3,6 +3,12 @@ import Aux from '../hoc/Aux';
 import Burger from '../components/Burger/Burger';
 import BuildControls from '../components/Burger/BuildControls/BuildControls';
 
+const ingredient_prices={
+  salad:0.5,
+  bacon:1.2,
+  cheese:0.6,
+  meat:2
+}
 class BurgerBuilder extends Component {
   state={
     ingredients:{
@@ -10,13 +16,48 @@ class BurgerBuilder extends Component {
         bacon:0,
         cheese:0,
         meat:0
-    }
+    },
+    totalPrice:4,
+  }
+  addIngredientHandler=(type)=>{
+    const oldCount=this.state.ingredients[type];
+    const updatedCount=oldCount+1;
+    const updatedIngredients={
+      ...this.state.ingredients
+    };
+    updatedIngredients[type]=updatedCount;
+    const priceAddition=ingredient_prices[type]
+    const oldPrice=this.state.totalPrice;
+    const newPrice=oldPrice+priceAddition;
+    this.setState({totalPrice:newPrice,ingredients:updatedIngredients})
+  }
+  removeIngredientHandler=(type)=>{
+    const oldCount=this.state.ingredients[type];
+    if(oldCount<=0){return;}
+    const updatedCount=oldCount-1;
+    const updatedIngredients={
+      ...this.state.ingredients
+    };
+    updatedIngredients[type]=updatedCount;
+    const priceDeduction=ingredient_prices[type]
+    const oldPrice=this.state.totalPrice;
+    const newPrice=oldPrice- priceDeduction;
+    this.setState({totalPrice:newPrice,ingredients:updatedIngredients})
   }
 render(){
+  const disabledInfo={
+    ...this.state.ingredients
+  }
+  for(let key in disabledInfo){
+    disabledInfo[key]=disabledInfo[key]<=0;
+  }
   return(
     <Aux>
       <Burger ingredients={this.state.ingredients}/>
-      <BuildControls/>
+      <BuildControls
+        ingredientAdded={this.addIngredientHandler}
+        ingredientRemoved={this.removeIngredientHandler}
+        disabled={disabledInfo}/>
     </Aux>
   )
 }
