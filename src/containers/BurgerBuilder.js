8f2f1fbeec35ac8cb9dcb60527720fd31b4d6ce6
Łukasz Ustach <lukasz.ustach@gmail.com>
2018-08-10 20:16:50 +0200

    update backdrop,add button&summary

--- src/containers/BurgerBuilder.js
+++ src/containers/BurgerBuilder.js
@@ -62,6 +62,12 @@ class BurgerBuilder extends Component {
   purchaseHandler=()=>{
     this.setState({purchasing:true})
   }
+  purchaseCancelHandler=()=>{
+    this.setState({purchasing:false});
+  }
+  purchaseContinueHandler=()=>{
+    alert('You continue!');
+  }
 render(){
   const disabledInfo={
     ...this.state.ingredients
@@ -71,8 +77,12 @@ render(){
   }
   return(
     <Aux>
-      <Modal show={this.state.purchasing}>
-        <OrderSummary ingredients={this.state.ingredients}/>
+      <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+        <OrderSummary
+          ingredients={this.state.ingredients}
+          price={this.state.totalPrice}
+          purchaseCancelled={this.purchaseCancelHandler}
+          purchaseContinued={this.purchaseContinueHandler}/>
     </Modal>
       <Burger ingredients={this.state.ingredients}/>
       <BuildControls
