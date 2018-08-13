c67c2c2c063c700a3cf47596a9debf7447bda729
Łukasz Ustach <lukasz.ustach@gmail.com>
2018-08-13 13:59:46 +0200

    nav,logo,

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css'
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import Sidedrawer from '../Navigation/Sidedrawer/Sidedrawer';
 
 const layout=(props)=>(
   <Aux>
-  <div>Toolbar,sidedrawer,backdrop</div>
+  <Toolbar />
+  <Sidedrawer />
   <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import BurgerLogo from '../../assets/images/burger-logo.png';
+import classes from './Logo.css';
+
+const logo=(props)=>(
+  <div className={classes.Logo}>
+    <img src={BurgerLogo} alt='burger' />
+  </div>
+)
+export default logo;
